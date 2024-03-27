696ebaa9649a7af6eef56a51af2ab967c438b75e
Raphael M. Santana <raphael@axiontecnologia.com.br>
2024-03-27 16:17:16 -0300

    Adicionar arquivos de projeto.

---
+++ AppFinanceiro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppFinanceiro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
